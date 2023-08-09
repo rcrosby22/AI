8a99c3fe5a1c5d9fc9d4860180927654dc3e2597
Ryan Crosby <ryan.crosby22@gmail.com>
2023-08-09 09:52:07 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Hero from './components/Hero.jsx'
-import Demo from './components/Demo.jsx'
+import Hero from './components/Hero'
+import Demo from './components/Demo'
 import './App.css'
 const App = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app'
-import { Provider } from 'react-redux'
-import { store } from './services/store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import { store } from "./services/store";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
